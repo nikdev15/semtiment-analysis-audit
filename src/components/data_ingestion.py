d75a09af5fc7d75fbcfeab5b0379f9fa234c7d77
Nikhil <nikhilisaac1519@gmail.com>
2025-01-14 19:17:42 +0530

    data ingestion done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,20 +3,20 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+from datasets import load_dataset
 
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+#from src.components.data_transformation import DataTransformation
+#from src.components.data_transformation import DataTransformationConfig
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
+#from src.components.model_trainer import ModelTrainerConfig
+#from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.parquet")
+    test_data_path: str=os.path.join('artifacts',"test.parquet")
+    raw_data_path: str=os.path.join('artifacts',"data.parquet")
 
 class DataIngestion:
     def __init__(self):
@@ -25,21 +25,20 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\StudentsPerformance.csv')
+            df=load_dataset("FinanceInc/auditor_sentiment")
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            #df.to_parquet(self.ingestion_config.raw_data_path,header=True)
+            train_set = df['train']
+            test_set = df['test']
 
-            logging.info("Train test split initiated")
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_parquet(self.ingestion_config.train_data_path)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_parquet(self.ingestion_config.test_data_path)
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-            logging.info("Ingestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -53,8 +52,8 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    #data_transformation=DataTransformation()
+    #train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    #modeltrainer=ModelTrainer()
+    #print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
